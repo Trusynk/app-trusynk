d818fa03fa34ef9f4d4430d325f3e3fefac064b4
testboiz <tvmini0202@gmail.com>
2025-10-22 19:16:39 +0700

    feat : add login modal and navbar

--- app/app.vue
+++ app/app.vue
@@ -1,8 +1,47 @@
+<script setup lang="ts">
+import type { AuthFormField } from "@nuxt/ui";
+
+const fields: AuthFormField[] = [
+  {
+    name: "email",
+    type: "email",
+    label: "Email",
+    placeholder: "Enter your email",
+    required: true,
+  },
+];
+
+const providers = [
+  {
+    label: "Google",
+    icon: "i-simple-icons-google",
+    onClick: () => {
+      console.log("unimplemented");
+    },
+  },
+];
+</script>
+
 <template>
-    <UApp>
-        <div>
-            <NuxtRouteAnnouncer />
-            <NuxtWelcome />
-        </div>
-    </UApp>
+  <UApp>
+    <UHeader>
+      <template #title> Trusynk </template>
+
+      <template #right>
+        <UModal :ui="{ content: 'w-1/4' }">
+          <UButton label="Login" color="neutral" variant="outline" />
+          <template #content>
+            <div class="flex flex-col items-center justify-center gap-4 p-4">
+              <UAuthForm
+                title="Login"
+                description="Log in with Google or Insert your email"
+                :fields="fields"
+                :providers="providers"
+              />
+            </div>
+          </template>
+        </UModal>
+      </template>
+    </UHeader>
+  </UApp>
 </template>
