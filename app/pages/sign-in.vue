d3f27fa6da10edf80a2cbdc76c3748bf405bec33
testboiz <tvmini0202@gmail.com>
2025-10-23 12:34:23 +0700

    feat : make register page

---
+++ app/pages/sign-in.vue
@@ -0,0 +1,48 @@
+<script setup lang="ts">
+import type { AuthFormField } from "@nuxt/ui";
+
+const fields: AuthFormField[] = [
+  {
+    name: "password",
+    type: "password",
+    label: "Password",
+    placeholder: "Enter your password",
+    required: true,
+  },
+  {
+    name: "company",
+    type: "text",
+    label: "Company",
+    placeholder: "Trusynk",
+    required: true,
+  },
+];
+
+const providers = [
+  {
+    label: "Google",
+    icon: "i-simple-icons-google",
+    onClick: () => {
+      console.log("unimplemented");
+    },
+  },
+];
+</script>
+
+<template>
+  <UApp>
+    <UHeader>
+      <template #title> Trusynk </template>
+    </UHeader>
+    <div
+      class="flex flex-col items-center justify-center gap-4 p-4 max-w-lg mx-auto"
+    >
+      <UAuthForm
+        title="Register"
+        description="Register your account with Google or use password"
+        :fields="fields"
+        :providers="providers"
+      />
+    </div>
+  </UApp>
+</template>
