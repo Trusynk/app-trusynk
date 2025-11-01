4ef1b8101f7c25021635ec3d5780ac1aa0da9bf6
testboiz <tvmini0202@gmail.com>
2025-11-01 18:49:33 +0700

    feat (index) : add non logged in interface

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -5,22 +5,34 @@ import logoUrl from "../../public/light-logomark.png";
 import z from "zod";
 const { $supabase } = useNuxtApp();
 const loading = ref(true);
-
+const logged_in = ref(false);
 const isHydrated = ref(false);
 
 // TODO : use as dashboard
 const { data, error } = await $supabase.auth.getSession();
 if (data.session) {
   loading.value = false;
+  logged_in.value = true;
 } else {
   loading.value = false;
 
   if (error) {
-    console.log(error);
+    throw createError({ status: 404, message: "Data does not exists" });
   } else {
-    console.log("No user logged in");
+    logged_in.value = false;
   }
 }
+const nullUuid = "00000000-0000-0000-0000-000000000000";
+const { data: userData, error: userError } = await $supabase
+  .from("user")
+  .select("*")
+  .eq("uiUserId", data.session?.user.id || nullUuid);
+
+if (userError) {
+  throw createError({ status: 404, message: userError.message });
+}
+
+const displayData = userData[0];
 
 const fields: AuthFormField[] = [
   {
@@ -46,18 +58,18 @@ const schema = z.object({
   email: z.email("Invalid email"),
 });
 
-const dbValueLogin = {
-  txFName: "Andrew",
-  txLName: "V",
-  txBusinessName: "Trusynk",
-  txEmail: "connect@trusynk.com",
-  txPhoneNumber: "+6287788895099",
-  txWebsiteUrl: "www.trusynk.com",
-  txInstagramURL: "instagram.com/trusynk",
-  txLinkedinURL: "linkedin.com/company/trusynk",
-  txFacebookURL: "facebook.com/trusynk",
-  txThreadsURL: undefined,
-};
+// const dbValueLogin = {
+//   txFName: "Andrew",
+//   txLName: "V",
+//   txBusinessName: "Trusynk",
+//   txEmail: "connect@trusynk.com",
+//   txPhoneNumber: "+6287788895099",
+//   txWebsiteUrl: "www.trusynk.com",
+//   txInstagramURL: "instagram.com/trusynk",
+//   txLinkedinURL: "linkedin.com/company/trusynk",
+//   txFacebookURL: "facebook.com/trusynk",
+//   txThreadsURL: undefined,
+// };
 
 const keyMap: Record<string, string> = {
   txBusinessName: "Business Name",
@@ -119,27 +131,47 @@ async function onSubmit(payload: FormSubmitEvent<Schema>) {
         <template #title> Trusynk </template>
 
         <template #right>
-          <UButton
-            label="Dashboard"
-            color="neutral"
-            variant="outline"
-            @click="navigateTo('/dashboard')"
-          />
+          <div v-if="!logged_in">
+            <UModal :ui="{ content: 'w-1/4' }">
+              <UButton label="Login" color="neutral" variant="outline" />
+              <template #content>
+                <div
+                  class="flex flex-col items-center justify-center gap-4 p-4"
+                >
+                  <UAuthForm
+                    title="Login"
+                    description="Log in with Google or Insert your email"
+                    :fields="fields"
+                    :providers="providers"
+                    @submit="onSubmit"
+                  />
+                </div>
+              </template>
+            </UModal>
+          </div>
+          <div v-else>
+            <UButton
+              label="Dashboard"
+              color="neutral"
+              variant="outline"
+              @click="navigateTo('/dashboard')"
+            />
+          </div>
         </template>
       </UHeader>
       <USeparator class="opacity-0 h-10" />
       <div class="max-w-lg mx-auto px-4">
         <UCard variant="subtle">
           <template #default>
-            <div class="flex flex-col p-2">
-              <div v-if="loading || !isHydrated" class="w-24 h-24 self-center">
+            <div v-if="displayData !== undefined" class="flex flex-col p-2">
+              <div v-if="loading || !isHydrated" class="w-24 h-24 mx-auto">
                 <UAvatar
                   icon="i-lucide-hourglass"
                   size="3xl"
                   class="w-full h-full"
                 />
               </div>
-              <div v-else class="w-24 h-24 self-center">
+              <div v-else class="w-24 h-24 mx-auto">
                 <UAvatar
                   :src="logoUrl"
                   alt="T"
@@ -158,61 +190,64 @@ async function onSubmit(payload: FormSubmitEvent<Schema>) {
                   <div class="flex justify-between">
                     <div class="text-lg ltr">Name</div>
                     <div class="text-lg text-right rtl">
-                      {{ dbValueLogin.txFName }} {{ dbValueLogin.txLName }}
+                      {{ displayData.txFName }} {{ displayData.txLName }}
                     </div>
                   </div>
                   <div>
                     <div
-                      v-for="(value, key, index) in dbValueLogin"
+                      v-for="(value, key, index) in displayData"
                       :key="`${key}-${index}-${String(value)}`"
                       class="flex justify-between"
                     >
-                      <div
-                        v-if="dbValueLogin[key] !== undefined"
-                        class="text-lg ltr"
-                      >
-                        {{ keyMap[key] }}
-                      </div>
+                      <template v-if="keyMap[key] && value">
+                        <div class="text-lg ltr">
+                          {{ keyMap[key] }}
+                        </div>
 
-                      <div class="text-lg text-right rtl">
-                        <template v-if="key === 'txFName' || key === 'txLName'">
-                          <!-- ignore case -->
-                        </template>
-
-                        <template v-else-if="key === 'txEmail'">
-                          <a
-                            :href="`mailto:${value}`"
-                            class="text-blue-500 underline"
-                            >{{ value }}</a
+                        <div class="text-lg text-right rtl">
+                          <template
+                            v-if="key === 'txFName' || key === 'txLName'"
                           >
-                        </template>
+                            <!-- ignore case -->
+                          </template>
 
-                        <template v-else-if="key === 'txPhoneNumber'">
-                          <a
-                            :href="`tel:${value}`"
-                            class="text-blue-500 underline"
-                            >{{ value }}</a
-                          >
-                        </template>
+                          <template v-else-if="key === 'txEmail'">
+                            <a
+                              :href="`mailto:${value}`"
+                              class="text-blue-500 underline"
+                              >{{ value }}</a
+                            >
+                          </template>
 
-                        <template v-else-if="key.toLowerCase().includes('url')">
-                          <a
-                            :href="
-                              value?.startsWith('http')
-                                ? value
-                                : 'https://' + value
-                            "
-                            target="_blank"
-                            class="text-blue-500 underline"
+                          <template v-else-if="key === 'txPhoneNumber'">
+                            <a
+                              :href="`tel:${value}`"
+                              class="text-blue-500 underline"
+                              >{{ value }}</a
+                            >
+                          </template>
+
+                          <template
+                            v-else-if="key.toLowerCase().includes('url')"
                           >
+                            <a
+                              :href="
+                                (value as string)?.startsWith('http')
+                                  ? (value as string)
+                                  : 'https://' + value
+                              "
+                              target="_blank"
+                              class="text-blue-500 underline"
+                            >
+                              {{ value }}
+                            </a>
+                          </template>
+
+                          <template v-else-if="keyMap[key]">
                             {{ value }}
-                          </a>
-                        </template>
-
-                        <template v-else>
-                          {{ value }}
-                        </template>
-                      </div>
+                          </template>
+                        </div>
+                      </template>
                     </div>
                   </div>
                 </div>
@@ -227,6 +262,20 @@ async function onSubmit(payload: FormSubmitEvent<Schema>) {
                   >
                 </div>
               </div>
+            </div>
+            <div v-else-if="logged_in && !userData">
+              <div class="text-xl text-center font-bold my-2">Trusynk</div>
+              <div class="text-center">
+                Visit the dashboard to add your card information
+              </div>
+            </div>
+            <div v-else-if="!isHydrated">
+              <div class="text-xl text-center font-bold my-2">Trusynk</div>
+              <div class="text-center">Loading...</div>
+            </div>
+            <div v-else>
+              <div class="text-xl text-center font-bold my-2">Trusynk</div>
+              <div class="text-center">Log in to view your card</div>
             </div>
           </template>
         </UCard>
